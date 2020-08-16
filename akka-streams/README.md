9fe60a879596cff0c6d4c68e7310409a6f7f32c3
Kamran Ali <kamranalinitb@gmail.com>
2020-08-16 11:15:23 +0530

    Added Akka Streams Project [Module]

---
+++ akka-streams/README.md
@@ -0,0 +1,5 @@
+# Akka Streams
+Akka Streams Code Snippets
+
+## Resources
+- [Akka Streams with Scala | Rock the JVM](https://www.udemy.com/course/akka-streams/)
---
+++ akka-streams/src/main/scala/playground/Playground.scala
@@ -0,0 +1,15 @@
+package playground
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+
+object Playground extends App {
+
+  implicit val actorSystem: ActorSystem = ActorSystem("playground")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  Source.single("Hello, Streams!").to(Sink.foreach(println)).run()
+
+
+}
